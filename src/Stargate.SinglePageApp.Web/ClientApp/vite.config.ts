65b834d30975744f949d0959442522ecf4cbe573
朱俊明 <libra.zhu@hotmail.com>
2022-03-13 23:39:04 +0800

    添加 PWA 支持。

--- src/Stargate.SinglePageApp.Web/ClientApp/vite.config.ts
+++ src/Stargate.SinglePageApp.Web/ClientApp/vite.config.ts
@@ -4,9 +4,26 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 
+import { VitePWA } from 'vite-plugin-pwa'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(),
+    VitePWA({
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Stargate 单网页应用',
+        short_name: 'Stargate SPA',
+        description: '采用 SPA 前端方式的 MVC 网站。',
+        theme_color: '#ffffff',
+        icons: [
+          { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' },
+          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
--- src/Stargate.SinglePageApp.Web/Program.cs
+++ src/Stargate.SinglePageApp.Web/Program.cs
@@ -17,7 +17,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 
-
+app.UseWelcomePage();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller}/{action=Index}/{id?}");
